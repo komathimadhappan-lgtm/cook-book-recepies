f9946ac8e5508fa36add80488260245e3f8710cb
komathimadhappan-lgtm <komathimadhappan@gmail.com>
2025-09-17 14:37:49 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,46 @@
+//Briyani cookbriyani Method in Javascript
+
+function cookbriyani() {
+let steps = [
+    "step 1: Wash and soak basmathi rice for 30 minutes.",
+    "step 2: Marinate chicken with yogurt, ginger-garlic past,tumeric,chilli powderand salt.",
+    "step 3: Heat oil/ghee in a pan ,fry onions until golden brown (brisita).",
+    "step 4: Add whole spices (bayleaf,cardamom,cinnamom,cloves).",
+    "step 5: Add marinated chicken and cook until half done.",
+    "step 6: Boil rice with salt and whole spices until 70% cooked drain.",
+    "step 7: In a heavy-bottom pot, layer rice and chicken alternately.",
+    "step 8: Sprinkle saffron milk and fried onions on top.",
+    "step 9: Cover with a tight lid (dum)and cook on lowflame for 20-25 minutes.",
+    "step 10: Serve hot with raita or salad!"
+];
+
+console.log(" Briyani Cooking Method /n");
+
+steps.forEach (step =>{
+    console.log(step);
+});
+
+//Ingredients list
+const Ingredients =[
+    "500g chicken (washed and cut into pieces)",
+    "2 cups basmathi rice (soaked for 30 mins)",
+    "3 large onions (thinly sliced)",
+    "2 tomatoes(chopped)",
+    "1/2 cup curd (yogurt)",
+    "2 tbsp ginger-garlic paste",
+    "3-4 green chilies (slit)",
+    "1/2 cup fresh coriander leaves",
+    "1/2 mini leaves",
+    "Whole spices (4 cloves,3 cardamoms,2 bay leaves 1 cinnamom stick)",
+    "2 tsp red chili powder",
+    "1 tsp turmeric powder",
+    "2 tsp garam masala",
+    "salt(as needed)",
+    "4 tbsp oil or ghee",
+    "4 cups water",
+];
+}
+//Run the program
+cookbriyani();
+
+
